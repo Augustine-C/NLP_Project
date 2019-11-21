784fb7f21b09f51138b2da859b62e4e0e79385bd
ArthurInTheShell <32620957+ArthurInTheShell@users.noreply.github.com>
2019-11-21 16:29:39 -0500

    NER & More Triple

--- src/main/java/nlptools/NLP_triple.java
+++ src/main/java/nlptools/NLP_triple.java
@@ -3,6 +3,8 @@ import edu.stanford.nlp.ie.util.RelationTriple;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreEntityMention;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
 import edu.stanford.nlp.simple.Sentence;
@@ -10,27 +12,33 @@ import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.ie.util.RelationTriple;
 import edu.stanford.nlp.simple.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class NLP_triple {
+
     public static void main(String[] args) throws Exception {
         // Create the Stanford CoreNLP pipeline
         Properties props = new Properties();
-        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie,ner");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
+        String testPathString = NLP_triple.class.getResource("/test.txt").getPath();
+        String docString = readFromTxt(testPathString);
+
         // Annotate an example document.
-        Annotation doc = new Annotation("Obama was happy. He is our president.");
+        Annotation doc = new Annotation(docString);
         pipeline.annotate(doc);
 
         // Loop over sentences in the document
         for (CoreMap sentence : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
             // Get the OpenIE triples for the sentence
-
             Collection<RelationTriple> triples =
-                    sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
+            sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
+
             // Print the triples
             for (RelationTriple triple : triples) {
                 System.out.println(triple.confidence + "\t" +
@@ -40,26 +48,35 @@ public class NLP_triple {
             }
         }
 
+        CoreDocument cdoc = new CoreDocument(docString);
+        pipeline.annotate(cdoc);
+        System.out.println("---");
+        System.out.println("entities found");
+        for (CoreEntityMention em : cdoc.entityMentions())
+            System.out.println("\tdetected entity: \t"+em.text()+"\t"+em.entityType());
+        System.out.println("---");
+        System.out.println("tokens and ner tags");
+        String tokensAndNERTags = cdoc.tokens().stream().map(token -> "("+token.word()+","+token.ner()+")").collect(
+                Collectors.joining(" "));
+        System.out.println(tokensAndNERTags);
 
-        Document doc1 = new Document("Arthur bought coffee. Coffee was bought by Arthur");
-        // Iterate over the sentences in the document
-        for (Sentence sent : doc1.sentences()) {
-            // Iterate over the triples in the sentence
-            for (RelationTriple triple : sent.openieTriples()) {
-                // Print the triple
-                System.out.println(triple.confidence + "\t" +
-                        triple.subjectLemmaGloss() + "\t" +
-                        triple.relationLemmaGloss() + "\t" +
-                        triple.objectLemmaGloss());
+    }
+
+    private static String readFromTxt(String path) {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(path));
+            StringBuilder content = new StringBuilder();
+
+            String s;
+            while ( (s = br.readLine()) != null) {
+                content.append(s+' ');
             }
-        }
-        List<String> result = new ArrayList<>();
-        for (CoreMap sentence : doc.get(CoreAnnotations.SentencesAnnotation.class)){
-            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)){
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                result.add(word);
-                System.out.println(word);
-            }
+            br.close();
+            return content.toString();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return "";
         }
     }
 
